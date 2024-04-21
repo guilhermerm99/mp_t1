66045c2faa8c5911b4d9da7cfd7d3285e7104568
Guilherme Ribeiro de Macedo <guilhermerm99@gmail.com>
2024-04-20 23:37:14 -0300

    Adiciona função auxiliar para obter o valor de um caractere romano
    Adiciona a função valor que retorna o valor decimal de um único caractere romano.

--- romanos.cpp
+++ romanos.cpp
@@ -1,4 +1,19 @@
 #include "romanos.hpp"
+#include <string.h
+
+int valor(char romano) {
+    switch (romano) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
 int romanos_para_decimal(char const * num_romano)
 {
   return 0; 
